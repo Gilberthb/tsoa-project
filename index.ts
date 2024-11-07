a49180f4f584f1843eb99be02b26930481590db5
Gilberthb <gilberthb704@gmail.com>
2024-11-07 09:18:23 +0200

    This is start up in express js

---
+++ index.ts
@@ -0,0 +1,13 @@
+// tsoa.json
+{
+    "entryFile": "src/app.ts",
+    "noImplicitAdditionalProperties": "throw-on-extras",
+    "controllerPathGlobs": ["src/**/*Controller.ts"],
+    "spec": {
+      "outputDirectory": "build",
+      "specVersion": 3
+    },
+    "routes": {
+      "routesDir": "build"
+    }
+  }
